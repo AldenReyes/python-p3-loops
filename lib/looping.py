9ebff9e12ed7b2cee41da0abe394ecb241fa068a
AldenReyes <122179486+AldenReyes@users.noreply.github.com>
2023-07-15 22:00:19 -0700

    Completes lab: practices basic loops with for and while constructs

--- lib/looping.py
+++ lib/looping.py
@@ -1,13 +1,30 @@
 #!/usr/bin/env python3
 
+
 def happy_new_year():
-    # code goes here!
-    pass
+    i = 10
+    while i >= 0:
+        print(i)
+        i -= 1
+    print("Happy New Year!")
+
 
 def square_integers(int_list):
-    # code goes here!
-    pass
+    square_ints = list()
+    for int in int_list:
+        square_int = int * int
+        square_ints.append(square_int)
+    return square_ints
+
 
 def fizzbuzz():
-    # code goes here!
-    pass
+    for i in range(1, 101):
+        str = ""
+        if i % 3 == 0:
+            str += "Fizz"
+        if i % 5 == 0:
+            str += "Buzz"
+        if str == "":
+            print(i)
+        else:
+            print(str)
